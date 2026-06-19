fa6c9a12e1976c2b9fbdd2b582e4a8fd2337e1ff
Devendra2005-n <dnimbalkar340@gmail.com>
2026-06-19 15:21:12 +0530

    I am Printing Table of 7

--- Dev.py
+++ Dev.py
@@ -1,4 +1,7 @@
-A=2
-B=5
-C=A+B
-print("The Sum of these Two Numbers is :-",C)
+# A=2
+# B=5
+# C=A+B
+# print("The Sum of these Two Numbers is :-",C)
+
+for i in range(7 , 71 ,7):
+ print("\n",i,"\n")
